95fbaf481fa9e5cb452dc9f16d4d879910c5d59c
Marina <5275747+freealise@users.noreply.github.com>
2022-07-17 20:19:05 +0300

    Update white-noise-processor.js

--- white-noise-processor.js
+++ white-noise-processor.js
@@ -20,23 +20,19 @@ class WhiteNoiseProcessor extends AudioWorkletProcessor {
   constructor() {
     super();
     this.indx = 0;
+    this.lastOut = 0.0;
   }
 
   process (inputs, outputs, parameters) {
     const output = outputs[0];
     output.forEach(channel => {
       for (let i = 0; i < channel.length; i++) {
-        //channel[i] = (Math.random() * 2 - 1) *
-        //  (parameters['customGain'].length > 1 ? parameters['customGain'][i] : parameters['customGain'][0])
+        channel[i] = (Math.random() * 2 - 1) *
+          (parameters['customGain'].length > 1 ? parameters['customGain'][i] : parameters['customGain'][0]);
         // note: a parameter contains an array of 128 values (one value for each of 128 samples),
         // however it may contain a single value which is to be used for all 128 samples
         // if no automation is scheduled for the moment.
-        channel[i] = 0;
-        for (var j=1; j<=48; j++) {
-          channel[i] += Math.sin( (this.indx+i) * Math.PI / 48000 * j *
-            (parameters['customFreq'].length > 1 ? parameters['customFreq'][i] : parameters['customFreq'][0])
-          ) / j / 4;
-        }
+        this.lastOut = channel[i];
       }
       this.indx += 128;
     });
