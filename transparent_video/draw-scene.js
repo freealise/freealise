9225e54b644f7e6d473762cc201f99625fc58010
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2024-10-12 23:51:16 +0300

    Update draw-scene.js

--- transparent_video/draw-scene.js
+++ transparent_video/draw-scene.js
@@ -1,5 +1,5 @@
 function drawScene(gl, programInfo, buffers, texture, cubeRotation) {
-  gl.clearColor(1.0, 1.0, 1.0, 1.0); // Clear to white, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 0.0); // Clear to transparent
   gl.clearDepth(1.0); // Clear everything
   gl.enable(gl.DEPTH_TEST); // Enable depth testing
   gl.depthFunc(gl.LEQUAL); // Near things obscure far things
