708aed63fa8e5b1c35a4f33c83868b5bb5d56387
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2024-10-13 07:36:43 +0300

    Update HelloJava.java

--- java/HelloJava.java
+++ java/HelloJava.java
@@ -2,6 +2,20 @@ public class HelloJava {
 
     public static void main(String[] args) {
         System.out.println("Hello " + args[0]);
+        System.out.println(GenerateMessage.generateMessage());
+        System.out.println(AnotherMessage.generateAnotherMessage());
     }
 
+}
+
+class GenerateMessage {
+    static String generateMessage() {
+        return "Here is one message";
+    }
+}
+ 
+class AnotherMessage {
+    static String generateAnotherMessage() {
+        return "Here is another message";
+    }
 }
