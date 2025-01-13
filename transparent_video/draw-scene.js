a18d465625f8c89b87361051886523e7f91390dd
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-01-13 17:51:38 +0300

    Update draw-scene.js

--- transparent_video/draw-scene.js
+++ transparent_video/draw-scene.js
@@ -103,7 +103,7 @@ function drawScene(gl, programInfo, buffers, texture, cubeRotation) {
   gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 
   {
-    const vertexCount = 720;
+    const vertexCount = 864;
     const type = gl.UNSIGNED_SHORT;
     const offset = 0;
     gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
