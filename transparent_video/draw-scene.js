f391de47e3ce56a482694ca2490d79962032a118
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-01-13 18:10:44 +0300

    Update draw-scene.js

--- transparent_video/draw-scene.js
+++ transparent_video/draw-scene.js
@@ -36,25 +36,25 @@ function drawScene(gl, programInfo, buffers, texture, cubeRotation) {
   mat4.translate(
     modelViewMatrix, // destination matrix
     modelViewMatrix, // matrix to translate
-    [-0.0, 0.0, -6.0]
+    [-0.0, 0.0, -0.0]
   ); // amount to translate
 
   mat4.rotate(
     modelViewMatrix, // destination matrix
     modelViewMatrix, // matrix to rotate
-    cubeRotation, // amount to rotate in radians
+    cubeRotation * 0.0, // amount to rotate in radians
     [0, 0, 1]
   ); // axis to rotate around (Z)
   mat4.rotate(
     modelViewMatrix, // destination matrix
     modelViewMatrix, // matrix to rotate
-    cubeRotation * 0.7, // amount to rotate in radians
+    cubeRotation, // amount to rotate in radians
     [0, 1, 0]
   ); // axis to rotate around (Y)
   mat4.rotate(
     modelViewMatrix, // destination matrix
     modelViewMatrix, // matrix to rotate
-    cubeRotation * 0.3, // amount to rotate in radians
+    cubeRotation * 0.0, // amount to rotate in radians
     [1, 0, 0]
   ); // axis to rotate around (X)
 
@@ -103,11 +103,14 @@ function drawScene(gl, programInfo, buffers, texture, cubeRotation) {
   gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 
   {
-    const vertexCount = 864;
     const type = gl.UNSIGNED_SHORT;
     const offset = 0;
-    gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
+    gl.drawElements(gl.TRIANGLES, vertexCount(12), type, offset);
   }
+}
+
+function vertexCount(div) {
+  return Math.pow(div, 2) * 6;
 }
 
 // Tell WebGL how to pull out the positions from the position
