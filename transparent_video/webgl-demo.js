f3416acb363785ec85bcc02d6620cf733dfb3da0
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-01-23 00:00:31 +0300

    Update webgl-demo.js

--- transparent_video/webgl-demo.js
+++ transparent_video/webgl-demo.js
@@ -391,6 +391,7 @@ document.querySelector("#glcanvas").addEventListener('click', function(e){
   if (gl.canvas.style.width == '320px') {
     gl.canvas.style.width = window.innerWidth + 'px';
     gl.canvas.style.height = window.innerHeight + 'px';
+    gl.canvas.requestFullScreen();
   } else {
     gl.canvas.style.width = '320px';
     gl.canvas.style.height = '240px';
