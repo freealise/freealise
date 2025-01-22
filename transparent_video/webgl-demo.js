ca30c0d9af6c544341601b1f610b35bca93ab48f
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-01-22 19:32:35 +0300

    Update webgl-demo.js

--- transparent_video/webgl-demo.js
+++ transparent_video/webgl-demo.js
@@ -355,3 +355,7 @@ document.querySelector("#glcanvas").addEventListener('pointerleave', function(e)
 document.querySelector("#glcanvas").addEventListener('pointercancel', function(e){
   md = false;
 });
+
+document.querySelector("#videoUrl").addEventListener('change', function(e){
+  main();
+});
