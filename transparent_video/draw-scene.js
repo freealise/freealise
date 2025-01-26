17afca6a5ce17d855b9793233c1ff49b492449a8
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-01-26 17:00:10 +0300

    Update draw-scene.js

--- transparent_video/draw-scene.js
+++ transparent_video/draw-scene.js
@@ -62,7 +62,7 @@ function drawScene(gl, programInfo, buffers, texture, cubeRotation, seg, pov) {
     modelViewMatrix, // destination matrix
     modelViewMatrix, // matrix to rotate
     cubeRotation.x * 0.005, // amount to rotate in radians
-    [Math.cos(pov.heading), 0, Math.sin(pov.heading)]
+    [Math.sin(pov.heading-Math.PI/2), 0, Math.cos(pov.heading-Math.PI/2)]
   ); // axis to rotate around (X)
   mat4.rotate(
     modelViewMatrix, // destination matrix
