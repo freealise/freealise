62962c14c4bf7b5529041a14cf9ffc6905021547
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-03-26 15:56:49 +0300

    Update interpolation.js

--- interpolation.js
+++ interpolation.js
@@ -12,3 +12,7 @@ function distance(p1, p2) {
 // https://paulbourke.net/miscellaneous/interpolation/
 function uniform_catmull_rom_spline(p0, p1, p2, p3, t) {return p1 + 0.5*t*(p2-p0 + t*(2*p0-5*p1+4*p2-p3 + t*(3*(p1-p2)+p3-p0)));}
 function lerp(y1, y2, mu) { return y1*(1-mu)+y2*mu; }
+
+/*
+lerp, smooth by moving average, lerp the difference from original points, add it to result
+*/
