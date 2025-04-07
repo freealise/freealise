2ac8f120083bcd6e237c0c0d5d94479b4a3c4f41
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-04-07 18:39:44 +0300

    Update interpolation.js

--- interpolation.js
+++ interpolation.js
@@ -14,3 +14,55 @@ function uniform_catmull_rom_spline(p0, p1, p2, p3, t) {return p1 + 0.5*t*(p2-p0
 function lerp(y1, y2, mu) { return y1*(1-mu)+y2*mu; }
 
 // lerp, smooth by moving average, lerp the difference from original points, add it to result  
+
+           try {
+				var w = 3;
+				var corner = 0.25;
+				var items = paper.project.getItems({ class: 'Group' });
+				var paths = items[items.length-1].getItems({ class: 'Path' });
+				for (var i=0; i<paths.length; i++) {
+					var diffs = [];
+					var corners = [];
+					for (var j=0; j<paths[i].segments.length; j++) {
+						var x = 0;
+					 var y = 0;
+						for (var k=j-parseInt(w/2); k<=j+parseInt(w/2); k++) {
+							if (k>=0 && k<paths[i].segments.length) {
+								x += paths[i].segments[k].point.x;
+								y += paths[i].segments[k].point.y;
+							} else if (k<0) {
+								x += paths[i].segments[paths[i].segments.length+k].point.x;
+								y += paths[i].segments[paths[i].segments.length+k].point.y;
+							} else if (k>=paths[i].segments.length) {
+								x += paths[i].segments[k-paths[i].segments.length].point.x;
+								y += paths[i].segments[k-paths[i].segments.length].point.y;
+							}
+						}
+						diffs[j] = Math.sqrt( Math.pow((paths[i].segments[j].point.x - x/w), 2) + Math.pow((paths[i].segments[j].point.y - y/w), 2) );;
+						if (diffs[j] >= Math.sqrt(2)*corner) {
+							corners[j] = true;
+						} else if (diffs[j] >= corner) {
+							corners[j] = null;
+							paths[i].segments[j].point.x = x/w;
+						 paths[i].segments[j].point.y = y/w;
+						} else {
+							corners[j] = false;
+							paths[i].segments[j].point.x = x/w;
+						 paths[i].segments[j].point.y = y/w;
+						}
+					}
+					var j=0;
+					while (paths[i].segments[j]) {
+						if (corners[j] === false) {
+							paths[i].segments.splice(j,1);
+							diffs.splice(j,1);
+							corners.splice(j,1);
+						} else if (corners[j] === true) {
+							paths[i].segments[j].clearHandles();
+							j++;
+						} else {
+							j++;
+						}
+					}
+				}
+			} catch(e) {alert(e);}
