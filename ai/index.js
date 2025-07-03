3a3b97471f22fe16a9e99ff21ee996e2fd4886f7
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-07-03 18:47:07 +0000

    Update index.js

--- ai/index.js
+++ ai/index.js
@@ -30,8 +30,8 @@ async function run() {
   model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
 
   // Generate some synthetic data for training. (y = 2x - 1)
-  const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-  const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+  const xs = tf.tensor2d(["a", "i", "e", "o", "u", "y"], [6, 1], "string");
+  const ys = tf.tensor2d(["A", "I", "E", "O", "U", "Y"], [6, 1], "string");
 
   // Train the model using the data.
   await model.fit(xs, ys, {epochs: 250});
@@ -39,7 +39,7 @@ async function run() {
   // Use the model to do inference on a data point the model hasn't seen.
   // Should print approximately 39.
   document.getElementById('micro-out-div').innerText =
-      model.predict(tf.tensor2d([20], [1, 1])).dataSync();
+      model.predict(tf.tensor2d(["ai"], [1, 1])).dataSync();
 }
 
 run();
