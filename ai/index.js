252edc4b226f6716473752817166549a4ec79cf4
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-07-03 19:04:43 +0000

    Update index.js

--- ai/index.js
+++ ai/index.js
@@ -30,8 +30,8 @@ async function run() {
   model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
 
   // Generate some synthetic data for training. (y = 2x - 1)
-  const xs = tf.tensor2d(["a", "i", "e", "o", "u", "y"], [6, 1], "string");
-  const ys = tf.tensor2d(["A", "I", "E", "O", "U", "Y"], [6, 1], "string");
+  const xs = tf.tensor2d(["a", "i", "e", "o", "u", "y"], [6, 1]);
+  const ys = tf.tensor2d(["A", "I", "E", "O", "U", "Y"], [6, 1]);
 
   // Train the model using the data.
   await model.fit(xs, ys, {epochs: 250});
@@ -42,4 +42,4 @@ async function run() {
       model.predict(tf.tensor2d(["ai"], [1, 1])).dataSync();
 }
 
-run();
+try{run();}catch(e){alert(e);}
