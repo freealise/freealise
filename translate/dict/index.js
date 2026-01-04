8967cb41372d4c691bd1a2646e042ccd8f2eb5e7
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2026-01-04 21:26:40 +0300

    Update index.js

--- translate/dict/index.js
+++ translate/dict/index.js
@@ -1,4 +1,4 @@
-const dir = "longman"
+const dir = "Longman_Dictionary_of_Contemporary_English_5th_Ed"
 const upload = document.getElementById('upload'); // File upload
 const bookname = document.getElementById('bookname'); // Dictionary title
 const inputContainer = document.getElementById('inputContainer'); // Search container
