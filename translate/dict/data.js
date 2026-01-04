3b56b061bfab1460807bd1597c11e60f5c63f127
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2026-01-04 19:28:48 +0300

    Save data.js.

---
+++ translate/dict/data.js
@@ -0,0 +1,22 @@
+const languages = {
+    English: {
+        name: "Oxford_Advanced_Learner_s_Dictionary",
+        synonyms: false,
+    },
+    Greek: {
+        name: "LiddellScott1940",
+        synonyms: true,
+    },
+    Latin: {
+        name: "LewisShort1879",
+        synonyms: false,
+    },
+    Abbreviations: {
+        name: "Elliott1998",
+        synonyms: false,
+    },
+    French: {
+        name: "FrenchWiktionary",
+        synonyms: true,
+    }
+};
